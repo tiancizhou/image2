c69d1aecbb7e5b030fc27a423827c2d1e7855174
ztc64003 <1294811773@qq.com>
2026-06-07 21:09:52 +0800

    Use anonymous profile display

--- miniprogram/pages/profile/profile.js
+++ miniprogram/pages/profile/profile.js
@@ -3,6 +3,9 @@ const { request, ensureLogin } = require('../../utils/api');
 Page({
   data: {
     userInfo: {},
+    displayName: '绘境用户',
+    userCode: '',
+    points: 0,
     checkedIn: false,
     showPoints: false,
     pointLogs: [],
@@ -17,7 +20,13 @@ Page({
       await ensureLogin();
       const profile = await request('/api/user/profile');
       const status = await request('/api/user/checkin/status');
-      this.setData({ userInfo: profile, checkedIn: status.checkedIn });
+      this.setData({
+        userInfo: profile,
+        displayName: `绘境用户 #${profile.id}`,
+        userCode: `UID ${profile.id}`,
+        points: profile.points || 0,
+        checkedIn: status.checkedIn,
+      });
     } catch {}
   },
 
@@ -36,7 +45,24 @@ Page({
     }
     try {
       const res = await request('/api/user/points?page=1&pageSize=20');
-      this.setData({ pointLogs: res.list, showPoints: true });
+      const pointLogs = res.list.map(item => ({
+        ...item,
+        typeText: this.formatLogType(item.type),
+        amountText: `${item.amount > 0 ? '+' : ''}${item.amount}`,
+        amountClass: item.amount > 0 ? 'positive' : 'negative',
+      }));
+      this.setData({ pointLogs, showPoints: true });
     } catch {}
+  },
+
+  formatLogType(type) {
+    const map = {
+      consume: '消费',
+      recharge: '充值',
+      checkin: '签到',
+      cdk: '兑换',
+      refund: '返还',
+    };
+    return map[type] || '积分';
   },
 });
